baa3f71f4814baac7361701e5d257f395d2fbc81
MaliaRK <160169166+MaliaRK@users.noreply.github.com>
2024-04-24 15:30:47 +0500

    amazing words counter app

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+console.log("\n***LET STARTS WORDS COUNTING***\n");
+const answer = await inquirer.prompt({
+    message: "Enter your sentence to count the words..  ",
+    type: 'input',
+    name: 'sentence'
+});
+const words = answer.sentence.trim().split(" ");
+console.log('\n', words);
+console.log('\nYour sentence words count is:', words.length);
